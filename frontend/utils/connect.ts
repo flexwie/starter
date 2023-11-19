b672a3220ca96b0f15c5230383ef3a1348f6bb3d
flexwie <mail@felixwie.com>
2023-11-19 22:18:38 +0100

    updated connect

--- frontend/utils/connect.ts
+++ frontend/utils/connect.ts
@@ -1,21 +1,23 @@
-import { headers } from "next/headers"
 import { createPromiseClient, Interceptor, PromiseClient } from "@bufbuild/connect"
 import { createGrpcTransport } from "@bufbuild/connect-node"
 import { ServiceType } from "@bufbuild/protobuf"
 
-const injectClientAuth: Interceptor = (next) => async (req) => {
-  const headerList = headers()
-  req.header.set('cookie', headerList.get('cookie')!)
-  return await next(req)
+const clientAuthInjectorFactory: (req: Request) => Interceptor = (request: Request) => {
+  const headerList = request.headers;
+
+  return (next) => async (req) => {
+    req.header.set('cookie', headerList.get('cookie')!)
+    return await next(req)
+  }
 }
 
-const transport = createGrpcTransport({
+const transportFactory = (request: Request) => createGrpcTransport({
   httpVersion: "2",
   baseUrl: "http://127.0.0.1:8080",
-  interceptors: [injectClientAuth],
+  interceptors: [clientAuthInjectorFactory(request)],
 })
 
 
-export function createClient<T extends ServiceType>(service: T): PromiseClient<T> {
-  return createPromiseClient(service, transport)
+export function createClient<T extends ServiceType>(service: T, request: Request): PromiseClient<T> {
+  return createPromiseClient(service, transportFactory(request))
 }
